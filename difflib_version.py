dead099fd45e3705bc2cf13b9dbce9c23f3edf02
zhongyuanli <zczlz91@ucl.ac.uk>
2021-07-15 13:46:19 +0800

    Update difflib_version.py

--- difflib_version.py
+++ difflib_version.py
@@ -1,32 +1,39 @@
 import difflib
 from pathlib import Path
 
+
+# load the file using Path, return the list of string
+def load_file(file_path):
+    file_context = Path(file_path)
+    string_list = file_context.read_text()
+    return string_list
+
+
+# define the differ used
 diff_module = difflib.Differ()
+# use html differ to generate html diagram
 html_diff = difflib.HtmlDiff()
 
+# get the context of two files
+dpct_version_string_list = load_file('main.dp.cpp')
+manual_modified_string_list = load_file('main.cpp')
+dpct_version_string_list = dpct_version_string_list.splitlines()
+manual_modified_string_list = manual_modified_string_list.splitlines()
 
-loads = ''
-with open('main.dp.cpp','r') as load:
-    loads = load.readlines()
-    load.close()
+# using the differ to generate the "differ" collection
+diff = difflib.context_diff(dpct_version_string_list, manual_modified_string_list)
 
-mems = ''
-with open('main.cpp', 'r') as mem:
-    mems = mem.readlines()
-    mem.close()
-
-f1 = open("main.cpp",'r',encoding="UTF-8")
-f2 = open("main.dp.cpp",'r',encoding="UTF-8")
-contents1 = Path('main.cpp').read_text()
-contents2 = Path('main.dp.cpp').read_text()
-
-
-diff = difflib.context_diff(contents1.splitlines(),contents2.splitlines())
-
-result_collection = []
+# define the differ_collection
+preprocessing_diff_collection = []
 for line in diff:
-    result_collection.append(line)
-print(diff)
+    diff_item = str(line)
+    preprocessing_diff_collection.append(diff_item)
+
+# result_collection = ""
+# for line in diff:
+#     result_collection += str(line)
+# print(result_collection.split('\n'))
+
 #
 # with open('htmlout.html','a+') as fo:
 #     fo.write(html_diff.make_file(loads,mems))
