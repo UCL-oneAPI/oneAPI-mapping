2309fcc6da724acd8c8a985a118ff350e3eadea8
zhongyuanli <zczlz91@ucl.ac.uk>
2021-07-15 09:17:50 +0800

    upload files add context differ

--- difflib_version.py
+++ difflib_version.py
@@ -1,7 +1,9 @@
 import difflib
+from pathlib import Path
 
 diff_module = difflib.Differ()
 html_diff = difflib.HtmlDiff()
+
 
 loads = ''
 with open('main.dp.cpp','r') as load:
@@ -13,9 +15,19 @@ with open('main.cpp', 'r') as mem:
     mems = mem.readlines()
     mem.close()
 
-diff = diff_module.compare(loads,mems)
-print('\n'.join(diff))
+f1 = open("main.cpp",'r',encoding="UTF-8")
+f2 = open("main.dp.cpp",'r',encoding="UTF-8")
+contents1 = Path('main.cpp').read_text()
+contents2 = Path('main.dp.cpp').read_text()
 
-with open('htmlout.html','a+') as fo:
-    fo.write(html_diff.make_file(loads,mems))
-    fo.close()
+
+diff = difflib.context_diff(contents1.splitlines(),contents2.splitlines())
+
+result_collection = []
+for line in diff:
+    result_collection.append(line)
+print(diff)
+#
+# with open('htmlout.html','a+') as fo:
+#     fo.write(html_diff.make_file(loads,mems))
+#     fo.close()
